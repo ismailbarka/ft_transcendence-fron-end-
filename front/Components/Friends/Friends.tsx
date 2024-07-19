6d2eb1c079d60cd6d7d8ee6cde75543954767583
Ismail Barka <isbarka@e3r8p17.1337.ma>
2024-07-19 09:50:31 +0100

    Friends container in home bar

--- front/Components/Friends/Friends.tsx
+++ front/Components/Friends/Friends.tsx
@@ -1,10 +1,56 @@
 "use client"
 import React, { useState } from 'react'
 import classes from "./friends.module.css"
+import TestImage from '../../public/player.png'
+import Image from 'next/image';
 
 const buttons: string[] = ["Online", "Request", "Blocked"];
+const OnlinePeople = [{
+        name: "Ismail barka",
+        msg: "this will be the place of the last message from this person",
+        url: TestImage,
 
+    },{
+        name: "imrane barka",
+        msg: "this will be the place of the last message from this person",
+        url: TestImage,
+    },{
+        name: "anas barka",
+        msg: "this will be the place of the last message from this person",
+        url: TestImage,
+    },{
+        name: "Ismail barka",
+        msg: "this will be the place of the last message from this person",
+        url: TestImage,
+    }]
 const Friends = () => {
+
+    const Online =() =>{
+        return(
+            <div>
+                {OnlinePeople.map((item, index) =>{
+                    return  <div className={classes.Online} key={index}>
+                                <Image src={item.url} alt="" width={70} height={70} className={classes.image}/>
+                                <div className={classes.nameAndMsg}>
+                                    <h3 className={classes.name}>{item.name}</h3>
+                                    <p className={classes.message}>{item.msg}</p>
+                            </div>
+                </div>
+                })}
+                
+            </div>
+        )
+    }
+    const Request =() =>{
+        return(
+            <div>testRequest</div>
+        )
+    }
+    const Blocked =() =>{
+        return(
+            <div>testBlocked</div>
+        )
+    }
 
     const [clicked, setClicked] = useState("Online");
     const handleClick = (name:string) =>{
@@ -12,7 +58,14 @@ const Friends = () => {
         setClicked(name)
         console.log(name);
         console.log('====================================');
-
+    }
+    const handleData = (clicked:string) =>{
+        if(clicked === "Online")
+            return <Online/>
+        else if(clicked === "Request")
+            return <Request/>
+        else if(clicked === "Blocked")
+            return <Blocked/>
     }
   return (
     <div className={classes.Friends}>
@@ -23,8 +76,8 @@ const Friends = () => {
                 )
             })}        
         </div>
-        <div>
-            asd
+        <div className={classes.data}>
+            {handleData(clicked)}
         </div>
     </div>
   )
